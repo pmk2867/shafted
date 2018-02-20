cbae08174edada2683ad6f762ad9d3dcf2402daa
Phill Kelner <pmk2867@rit.edu>
2018-02-20 18:30:49 +0000

    Complete the torque read command
    Also added some print statements to allow for easy viewing of
    successful (or not) transmission.

--- shafted/serialtest.py
+++ shafted/serialtest.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+import time
 import serial
 import spidev
 
-port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
+port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=0.010)
 spi = spidev.SpiDev()
 
 spi.open(0, 0)
@@ -13,15 +14,33 @@ def send_recv_speed(speed):
     msb = speed >> 8
     lsb = speed & 0xFF
     response = spi.xfer2([msb, lsb])
-    inboard = response[0] >> 8 + response[1]
-    outboard = response[2] >> 8 + response[3]
+    if len(response) != 4:
+        return None, None
+    else:
+        inboard = response[0] >> 8 + response[1]
+        outboard = response[2] >> 8 + response[3]
 
     return inboard, outboard
 
 def send_recv_torque(torque):
+    torque = hex(torque)
+    port.write(torque)
+    rcv = port.read(4)
+    if len(rcv) != 4:
+        return None, None
+    else:
+        inboard = int(rcv[0]) >> 8 + int(rcv[1])
+        outboard = int(rcv[2]) >> 8 + int(rcv[3])
 
+    return inboard, outboard
 
 while True:
-    port.write("\r\nSay something:")
-    rcv = port.read(10)
-    port.write("\r\nYou sent:" + repr(rcv))
+    time.sleep(0.009)
+
+    in_s, out_s = send_recv_speed(600)
+    in_t, out_t = send_recv_torque(20)
+
+    print("    Speed\t|   Torque")
+    print("In\tOut\t|In\tOut")
+    print(str(in_s) + "\t" + str(out_s) + "\t|" + str(in_t) + "\t" + str(out_t) + "\t")
+    print("\n\r")

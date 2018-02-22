bd942980776faba6215afc6598b5f443aa97080c
Unknown <mjk9135@rit.edu>
2018-02-22 11:13:08 -0500

    Added ability to respond to RPi requests, general cleanup/commenting
    Still requires testing w/RPi

--- shafted/Teensey/torque_controller/torque_controller.ino
+++ shafted/Teensey/torque_controller/torque_controller.ino
@@ -1,96 +1,99 @@
+/* Torque Teensy Controller Code
+ *  Action Items to be completed related to this code by priority
+ *  1) Test to determine if the code responds to RPi requests
+ *  2) Fix software issue preventing data from being read accurately from both inboard 
+ *     and outboard simultaneously
+ *  3) Write code to control Linear Actuator based on RPi torque setpoint 
+ *  
+ */
+
+
 //Libraries
 #include <TimeLib.h>
-#include <Wire.h>
-//#include <uart.h>
+#include <Wire.h> //i2c library
 
 //Defining Pins
-#define I2C_SCL 19
+#define I2C_SCL 19 //i2c clock and data pins
 #define I2C_SDA 18
 
-
-
 //Global Variables
-int setpoint = 0;
-uint16_t actual_in = 0;
-uint16_t actual_out = 0;
+uint8_t t_setpoint = 0; //torque setpoint variable
+
+uint16_t raw_inboard_data = 0; //inboard and outboard adc data
+uint16_t raw_outboard_data = 0; 
+
 int time = 0;
 int prev_time = 0;
 
-//Defining ADS registers/commands
-byte Set_Inboard[2];
-
+byte Set_Inboard[2]; //2 byte arrays to be written to adc configuration register 
 byte Set_Outboard[2];
 
-
 void setup() {
-  //Initialize Serial
+  //Initialize Serial ports
   Serial.begin(9600);
   Serial1.begin(115200);
-  //uart_init(9600);
-
-  delay(5000); //time to open serial monitor
   
-  // Initiate Wire Library
+  //Initiate Wire (i2c) Library
   Wire.begin();
 
   //Pin Setup
-  Wire.setSDA(I2C_SDA);
+  Wire.setSDA(I2C_SDA); 
   Wire.setSCL(I2C_SCL);
   
-  // Enable Measurement
-  Wire.beginTransmission(0x48);
+  //Enable i2c Measurement
+  Wire.beginTransmission(0x48); //0x48 = adc i2c address
   Wire.endTransmission(); 
 
-  Set_Inboard[0] = 0x81;
+  //Set Configuration Register Arrays
+  Set_Inboard[0] = 0x81; //Bytes to be written that set Conversion Register to show Inboard Data
   Set_Inboard[1] = 0x83;
-
-  Set_Outboard[0] = 0xB1;
+  Set_Outboard[0] = 0xB1; //Bytes to be written that set Conversion Register to show Outboard Data 
   Set_Outboard[1] = 0x83;
 }
 
-
-uint16_t Read_Inboard_Torque() {
-  //Configure ADS to show outboard torque data
+//Data Acquisition Functions
+uint16_t Read_Inboard_Torque() { // Reads Inboard data
+  //Configure adc to show inboard data
   Wire.beginTransmission(byte(0x48)); 
-  Wire.write(byte(0x01)); 
-  Wire.write(Set_Inboard,2);
+  Wire.write(byte(0x01)); //config register address
+  Wire.write(Set_Inboard,2); //Sets conversion register to show Inboard data
   Wire.endTransmission();
   //delay(2);
   //Ask register for data
   Wire.beginTransmission(byte(0x48));
-  Wire.write(byte(0x00));
-  Wire.endTransmission(); //End transmission, transmits data from register
-  Wire.requestFrom(0x48,2); //request the transmitted bytes from the register
+  Wire.write(byte(0x00)); //conversion register address
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x48,2); //request the transmitted bytes from the conversion register
   uint16_t result = (Wire.read() << 8) | Wire.read() ;// (high byte low byte)
- // delay(2);
+  //delay(2);
   return result;
 }
 
-/*uint16_t Read_Outboard_Torque() {
-  //Configure ADS to show outboard torque data
+uint16_t Read_Outboard_Torque() { // Reads Outboard data
+  //Configure adc to show outboard data
   Wire.beginTransmission(byte(0x48)); 
-  Wire.write(byte(0x01)); 
-  Wire.write(Set_Outboard,2);
+  Wire.write(byte(0x01)); //config register address
+  Wire.write(Set_Outboard,2); //Sets conversion register to show Outboard data
   Wire.endTransmission();
- // delay(2);
+  //delay(2);
   //Ask register for data
   Wire.beginTransmission(byte(0x48));
-  Wire.write(byte(0x00));
-  Wire.endTransmission(); //End transmission, transmits data from register
-  Wire.requestFrom(0x48,2); //request the transmitted bytes from the register
+  Wire.write(byte(0x00)); //conversion register address
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x48,2); //request the transmitted bytes from the conversion register
   uint16_t result = (Wire.read() << 8) | Wire.read() ;// (high byte low byte)
   //delay(2);
   return result;
-}*/
+}
 
-uint16_t Acquire_Setpoint() {
+/*uint16_t Acquire_t_setpoint() {
   int incomingMSB = 0;
-  int incomingLSB = 0;
+  int incomingLSB = 1;
   int incoming = 0;
   if (Serial1.available() > 1) {
-    /*setpoint = Serial1.read();
-    incomingLSB = setpoint & 0xFF;
-    incomingMSB = setpoint >> 8;*/
+    /*t_setpoint = Serial1.read();
+    incomingLSB = t_setpoint & 0xFF;
+    incomingMSB = t_setpoint >> 8;
     incoming = Serial1.read();
   }
   /*if (uart_available() > 0) {
@@ -98,17 +101,19 @@ uint16_t Acquire_Setpoint() {
   }
   if (uart_available() > 0) {
     incomingLSB = uart_getchar();
-  }*/
+  }
   uint16_t result = (incomingMSB << 8) | incomingLSB;
-  return result;
+  return result;*/
 }
 
-void Send_data (uint16_t out_torque, uint16_t in_torque) {
-  uint8_t outLSB = out_torque & 0xff;
+//Serial Monitor printing for testing purposes
+void Print_data (uint16_t out_torque, uint16_t in_torque) {
+  uint8_t outLSB = out_torque & 0xff; //data processing for transmission
   uint8_t outMSB = (out_torque >> 8);
   uint8_t inLSB = in_torque & 0xff;
   uint8_t inMSB = (in_torque >> 8);
-  Serial.println(" ");
+  
+  Serial.println(" "); //Printing to serial monitor
   Serial.print("Inboard MSB = ");
   Serial.print(inMSB);
   Serial.print("   Inboard LSB = ");
@@ -118,30 +123,40 @@ void Send_data (uint16_t out_torque, uint16_t in_torque) {
   Serial.print(outMSB);
   Serial.print("   Outbboard LSB = ");
   Serial.print(outLSB);
-  Serial1.write(inMSB);
-  Serial1.write(inLSB);
-  Serial1.write(outMSB);
-  Serial1.write(outLSB);
-  /*uart_putchar(inMSB);
-  uart_putchar(inLSB);
-  uart_putchar(outMSB);
-  uart_putchar(outLSB);*/
 }
 
 void loop() {
-  //Acquire Relevant Data
   time  = millis() - prev_time;
-  actual_in = Read_Inboard_Torque();
-  //actual_out = Read_Outboard_Torque();
+  raw_inboard_data = Read_Inboard_Torque();
+  raw_outboard_data = Read_Outboard_Torque();
+  //prints data to serial monitor every 10ms
   if (time > 10) {
-     setpoint = Acquire_Setpoint();
-     Serial.println(" ");
-     Serial.print(setpoint);
-     Send_data(actual_out, actual_in);
+     //t_setpoint = Acquire_t_setpoint();
+     //Serial.println(" ");
+     //Serial.print(t_setpoint);
+     Print_data(raw_outboard_data, raw_inboard_data);
      Serial.println(" ");
      Serial.print(millis());
      prev_time = millis();
   }
+} 
+
+void serialEvent() { //response to new data in RX buffer (RPi request)
+  while (Serial1.available()) {//checks RX buffer
+    t_setpoint = Serial1.read(); //Acquires setpoint
+    
+    uint8_t outLSB = raw_outboard_data & 0xff; //data processing for transmission
+    uint8_t outMSB = (raw_outboard_data >> 8);
+    uint8_t inLSB = raw_inboard_data_torque & 0xff;
+    uint8_t inMSB = (raw_inboard_data >> 8);
+    
+    Serial1.write(inMSB); //data transmission
+    Serial1.write(inLSB);
+    Serial1.write(outMSB);
+    Serial1.write(outLSB);
+  }
 }  
+
+
 
 
